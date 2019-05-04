c260cdd4d2787c23b138372c63bf73d5dc48eb67
Naraen Palanikumar <naraen.p@gmail.com>
2019-05-04 11:26:33 -0500

    revise

--- Collage.java
+++ Collage.java
@@ -2,12 +2,12 @@ public class Collage
 {
     public static void main (String[] args)
     {
-        Picture original = new Picture("collage/thonk.jpg");
-        Picture recursive = new Picture("collage/thonk.jpg");
-        Picture invert = new Picture("collage/thonk.jpg");
-        Picture gray = new Picture("collage/thonk.jpg");
-        Picture mVertical = new Picture("collage/thonk.jpg");
-        Picture rotate = new Picture("collage/thonk.jpg");
+        Picture original = new Picture("collage/thonk.png");
+        Picture recursive = new Picture("collage/thonk.png");
+        Picture invert = new Picture("collage/thonk.png");
+        Picture gray = new Picture("collage/thonk.png");
+        Picture mVertical = new Picture("collage/thonk.png");
+        Picture rotate = new Picture("collage/thonk.png");
         
         Picture canvas = new Picture("collage/canvas.jpg");
 
@@ -15,23 +15,23 @@ public class Collage
         canvas.copy(original,25,25);
 
         //copies recursive of picture onto original picture
-        canvas.copy(original,125, 25);
+        canvas.copy(original,225, 25);
         canvas.recursive(recursive,2,125,25);
 
         //inverts colors
         invert.invert();
-        canvas.copy(invert,225,25);
+        canvas.copy(invert,425,25);
 
         //converts to grey
         gray.gray();
-        canvas.copy(gray,25,125);
+        canvas.copy(gray,25,225);
 
         //mirrors over vertical line
         mVertical.mirrorVertical();
-        canvas.copy(mVertical,125,125);
+        canvas.copy(mVertical,225,225);
 
         //rotates image 90 degrees clockwise
-        canvas.rotate(rotate,225,125);
+        canvas.rotate(rotate,425,225);
 
         canvas.explore();
 
